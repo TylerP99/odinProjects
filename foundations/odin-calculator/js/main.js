5cdb443e78f35905a4b1496b2fe13d6211cfc95e
Tyler Perata <tylerperata@gmail.com>
2022-06-26 11:48:34 -0700

    Start js

---
+++ foundations/odin-calculator/js/main.js
@@ -0,0 +1,12 @@
+// Will make calculator "widget"
+//Can create a calculator "element" with js logic
+
+class Calculator {
+    consturctor() {
+        //What should this try to accomplish? Build the whole calculator here? Html and such? Add the css? Turn on logic? Yeah probably
+
+        //Create HTML to place in dom, add styles as needed, should be pretty barebones
+
+        //Turn on js parsing and such
+    }
+}
